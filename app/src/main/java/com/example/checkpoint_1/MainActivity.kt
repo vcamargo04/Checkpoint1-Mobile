da199b74c96f3a543ab7a480c5c01a3a7d03a3d9
gbalttazar <gbalttazar.sit@gmail.com>
2024-09-08 21:34:19 -0300

    Sua mensagem de commit aqui

--- app/src/main/java/com/example/checkpoint_1/MainActivity.kt
+++ app/src/main/java/com/example/checkpoint_1/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.checkpoint_1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val forgotPassword = findViewById<TextView>(R.id.textView3)
+        forgotPassword.setOnClickListener {
+            val intent = Intent(this@MainActivity, EsqueceuSenha::class.java)
+            startActivity(intent)
+        }
+
+        val integrantesButton = findViewById<Button>(R.id.button5)
+        integrantesButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, IntegrantesActivity::class.java)
+            startActivity(intent)
         }
     }
 }
